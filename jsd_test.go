b6fc71b418841478f3ef47f62d36559f75f419ac
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-11-06 23:32:55 +0900

    fix

--- jsd_test.go
+++ jsd_test.go
@@ -18,11 +18,11 @@ func TestDistance(t *testing.T) {
 		{"test004", "こんにちわ世界", "こんばんわ世界", 0.742},
 		{"test005", "こんにちわ世界", "こんにちわ", 0.838},
 		{"test006", "こんにちわ世界", "こんばんわ", 0.453},
-		{"test007", "こんにちわ世界", "世界", 0},
+		{"test007", "世界", "こんにちわ", 0},
+		{"test008", "こんにちわ世界", "", 0},
 	}
 
 	for _, test := range tests {
-		println(test.name)
 		got := StringDistance(test.lhs, test.rhs)
 		if math.Abs(got-test.want) > 0.001 {
 			t.Errorf("%v: want %v but %v: %v vs %v", test.name, test.want, got, test.lhs, test.rhs)
